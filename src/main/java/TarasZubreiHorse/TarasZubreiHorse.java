30516d9272a3c94a98cbfb2a290fe3bbd49846d1
Taras Zubrei <taraszubrei@gmail.com>
2016-04-11 13:06:45 +0300

    +Taras Zubrei final version

--- src/main/java/TarasZubreiHorse/TarasZubreiHorse.java
+++ src/main/java/TarasZubreiHorse/TarasZubreiHorse.java
@@ -12,12 +12,10 @@ public class TarasZubreiHorse {
         Calc calc = new Calculator2(parser.parse(board), actor);
         List<Point> points = calc.find(parser.getStart());
         points.add(0, parser.getStart());
-        // using board
-        // return [[0,0], [1,2], ... ]
         int[][] result = new int[points.size()][2];
         for (int i = 0; i < points.size(); i++) {
             result[i] = new int[] {points.get(i).x, points.get(i).y};
         }
-        return new int[][]{};
+        return result;
     }
 }
