fcf3f757f227a3430b3f5c46fa094e5c1a14d830
Abrasha <aabrasha@gmail.com>
2016-04-21 14:11:14 +0300

    + encapsulated common test functions to parent class

---
+++ persistence/src/test/java/ConnectionAndSelectTestProvider.java
@@ -0,0 +1,69 @@
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class ConnectionAndSelectTestProvider {
+
+    protected Connection getConnection(DataSource dataSource) {
+
+        if (dataSource == null)
+            return null;
+
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+        } catch (SQLException e) {
+            System.out.println(e);
+            e.printStackTrace();
+            closeConnection(connection);
+        }
+        return connection;
+
+    }
+
+    protected boolean testSelect(Connection connection) {
+
+        boolean result = false;
+        Statement selectStatement = null;
+        ResultSet rs = null;
+        try {
+            selectStatement = connection.createStatement();
+            rs = selectStatement.executeQuery("SELECT * FROM ZOO.CLASSES");
+            result = rs.next();
+        } catch (SQLException e) {
+            System.out.println(e);
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (selectStatement != null) {
+                    selectStatement.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e1) {
+                System.out.println(e1);
+                e1.printStackTrace();
+            }
+        }
+
+        return result;
+
+    }
+
+    protected void closeConnection(Connection connection) {
+
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
--- persistence/src/test/java/XmlTestRunner.java
+++ persistence/src/test/java/XmlTestRunner.java
@@ -5,51 +5,46 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 import static org.springframework.util.Assert.isTrue;
 import static org.springframework.util.Assert.notNull;
 
-public class TestHsqldbConnection {
+public class XmlTestRunner extends ConnectionAndSelectTestProvider {
+
 
     private ClassPathXmlApplicationContext context;
     private Connection connection;
 
 
     @Before
-    public void setUp() throws SQLException {
+    public void setUp() {
         context = new ClassPathXmlApplicationContext("persistence-test-ctx.xml");
         context.getEnvironment().setActiveProfiles("dev");
         context.refresh();
 
         final DataSource dataSource = (DataSource) context.getBean("dataSource");
 
-        connection = dataSource.getConnection();
+        connection = super.getConnection(dataSource);
 
     }
 
 
     @Test
-    public void testConnection() throws SQLException {
+    public void testConnection() {
         notNull(connection, "Connection is not established.");
     }
 
     @Test
-    public void testQuery() throws SQLException {
-        final Statement selectStatement = connection.createStatement();
-        final ResultSet rs = selectStatement.executeQuery("SELECT * FROM ZOO.CLASSES");
+    public void testQuery() {
+        boolean selectIsSuccess = super.testSelect(connection);
 
-        isTrue(rs.next(), "Result set is empty.");
+        isTrue(selectIsSuccess, "Result set is empty.");
 
-        rs.close();
-        selectStatement.close();
     }
 
     @After
-    public void tearDown() throws SQLException {
-        connection.close();
+    public void tearDown() {
+        super.closeConnection(connection);
         context.close();
     }
 
