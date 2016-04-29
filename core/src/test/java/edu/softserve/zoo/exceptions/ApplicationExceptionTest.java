50e4c0768442eaf920fd784dc9374dab1c0a9e82
juliab <babenko.julia@gmail.com>
2016-04-29 12:47:19 +0300

    Exception builder modified to call protected/private constructor

--- core/src/test/java/edu/softserve/zoo/exceptions/ApplicationExceptionTest.java
+++ core/src/test/java/edu/softserve/zoo/exceptions/ApplicationExceptionTest.java
@@ -14,7 +14,7 @@ public final class ApplicationExceptionTest {
 
     private final static String MESSAGE = "Stub exception test message";
 
-    @SuppressWarnings({"ThrowableInstanceNeverThrown"})
+    @SuppressWarnings("ThrowableInstanceNeverThrown")
     private final static Throwable CAUSE = new Exception("Stub exception caused by this");
 
     @Test(expected = StubCustomException.class)
@@ -29,5 +29,16 @@ public final class ApplicationExceptionTest {
         assertNull("Exception is constructed with wrong reason", exception.getReason());
 
         throw exception;
+    }
+
+    @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
+    @Test
+    public void buildEmptyExceptionTest() {
+        final ApplicationException exception = ApplicationException.getBuilderFor(StubCustomException.class)
+                .build();
+
+        assertNull("Exception message should be null", exception.getMessage());
+        assertNull("Exception cause should be null", exception.getCause());
+        assertNull("Exception reason should be null", exception.getReason());
     }
 }
