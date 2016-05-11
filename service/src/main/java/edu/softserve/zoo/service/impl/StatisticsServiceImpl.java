c883b85af98b70c5a91514f20362e4d4af0685e8
Taras Zubrei <taraszubrei@gmail.com>
2016-05-11 13:44:28 +0300

    autowire repository

--- service/src/main/java/edu/softserve/zoo/service/impl/StatisticsServiceImpl.java
+++ service/src/main/java/edu/softserve/zoo/service/impl/StatisticsServiceImpl.java
@@ -3,9 +3,10 @@ package edu.softserve.zoo.service.impl;
 import edu.softserve.zoo.model.Employee;
 import edu.softserve.zoo.model.Statistics;
 import edu.softserve.zoo.model.Task;
-import edu.softserve.zoo.persistence.repository.impl.StatisticsRepositoryImpl;
+import edu.softserve.zoo.persistence.repository.StatisticsRepository;
 import edu.softserve.zoo.service.StatisticsService;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
@@ -15,29 +16,31 @@ import java.util.List;
  */
 @Transactional(readOnly = true)
 @Service
-public class StatisticsServiceImpl extends StatisticsRepositoryImpl implements StatisticsService {
+public class StatisticsServiceImpl implements StatisticsService {
+    @Autowired
+    StatisticsRepository repo;
 
     @Override
     public Float getFedAnimalsPercentage() {
-        return super.getFedAnimalsPercentage();
+        return repo.getFedAnimalsPercentage();
     }
 
     @Override
     public List<ImmutablePair<Task.TaskStatus.Status, Integer>> getEmployeeTasksStatuses(Integer id) {
         Employee employee = new Employee();
         employee.setId(id);
-        return super.getEmployeeTasksStatuses(employee);
+        return repo.getEmployeeTasksStatuses(employee);
     }
 
     @Override
     public List<ImmutablePair<Task.TaskType.Type, Integer>> getEmployeeTasksTypes(Integer id) {
         Employee employee = new Employee();
         employee.setId(id);
-        return super.getEmployeeTasksTypes(employee);
+        return repo.getEmployeeTasksTypes(employee);
     }
 
     @Override
     public Statistics getZooStatistics() {
-        return super.getZooStatistics();
+        return repo.getZooStatistics();
     }
 }
