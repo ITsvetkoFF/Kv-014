048cb59950cd248c2fd60e73aceb48c5b396d668
Mononofuu <mononofuu@gmail.com>
2016-05-12 00:44:23 +0300

    Animal related classes for persistence layer:  - Animal repository interface and implementation  - Animal specifications

---
+++ persistence/src/main/java/edu/softserve/zoo/persistence/specification/impl/animal/AnimalGetAllSpecification.java
@@ -0,0 +1,21 @@
+package edu.softserve.zoo.persistence.specification.impl.animal;
+
+import edu.softserve.zoo.model.Animal;
+import edu.softserve.zoo.persistence.specification.hibernate.CriterionSpecification;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ * @author Serhii Alekseichenko
+ */
+public class AnimalGetAllSpecification implements CriterionSpecification<Animal> {
+    @Override
+    public Class<Animal> getType() {
+        return Animal.class;
+    }
+
+    @Override
+    public Criterion query() {
+        return Restrictions.sqlRestriction("");
+    }
+}
