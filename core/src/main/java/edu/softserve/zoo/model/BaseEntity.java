e142b0aad41c11b1d33d29a62a6e54e0622ef43e
Mononofuu <mononofuu@gmail.com>
2016-05-12 20:24:21 +0300

    Change ID from INTEGER to LONG:  - DB creation script edit  - Base entity class edit

--- core/src/main/java/edu/softserve/zoo/model/BaseEntity.java
+++ core/src/main/java/edu/softserve/zoo/model/BaseEntity.java
@@ -1,16 +1,16 @@
 package edu.softserve.zoo.model;
 
 public abstract class BaseEntity {
-    private Integer id;
+    private Long id;
 
     public BaseEntity() {
     }
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
