55b9bb3ee5341accac9b88eac28f3dc59655204a
Mononofuu <mononofuu@gmail.com>
2016-05-17 12:47:12 +0300

    added:  - Animal repository  - Animal get by House specification  - Animal get by Species specification  - refactor

--- persistence/src/main/java/edu/softserve/zoo/persistence/provider/impl/DetachedCriteriaProcessingStrategy.java
+++ persistence/src/main/java/edu/softserve/zoo/persistence/provider/impl/DetachedCriteriaProcessingStrategy.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class DetachedCriteriaProcessingStrategy<T> implements SpecificationProcessingStrategy<T> {
 
     @Autowired
-    SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
     public DetachedCriteriaProcessingStrategy() {
 
