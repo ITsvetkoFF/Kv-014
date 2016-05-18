1e621d4895ed147fa23b3d165af64af2fbc1c80e
Mononofuu <mononofuu@gmail.com>
2016-05-19 02:06:44 +0300

    Added:  - Animal service layer (with two specific methods)  - Animal web layer (rest controller and dto)

--- web/src/main/java/edu/softserve/zoo/controller/rest/Routes.java
+++ web/src/main/java/edu/softserve/zoo/controller/rest/Routes.java
@@ -3,4 +3,5 @@ package edu.softserve.zoo.controller.rest;
 public interface Routes {
     String API_V1 = "api/v1";
     String GEO_ZONE = API_V1 + "/geo_zone";
+    String ANIMALS = API_V1 + "/animals";
 }
