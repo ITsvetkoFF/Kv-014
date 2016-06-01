4e5673484abd775735fa7d1679963e247a9a345e
Ilya Doroshenko <doroshenko.illia@gmail.com>
2016-06-01 16:11:26 +0300

    removed unnecessary controller, fixed specification

--- persistence/src/main/java/edu/softserve/zoo/persistence/specification/impl/employee/GetEmployeeByEmailSpecification.java
+++ persistence/src/main/java/edu/softserve/zoo/persistence/specification/impl/employee/GetEmployeeByEmailSpecification.java
@@ -1,9 +1,6 @@
 package edu.softserve.zoo.persistence.specification.impl.employee;
 
-import edu.softserve.zoo.model.Animal;
 import edu.softserve.zoo.model.Employee;
-import edu.softserve.zoo.model.Species;
-import edu.softserve.zoo.persistence.specification.Specification;
 import edu.softserve.zoo.persistence.specification.hibernate.DetachedCriteriaSpecification;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
@@ -11,9 +8,8 @@ import org.hibernate.criterion.Restrictions;
 /**
  * @author Ilya Doroshenko
  */
-
 public class GetEmployeeByEmailSpecification implements DetachedCriteriaSpecification<Employee> {
-    private static final String EMPLOYEE_EMAIL = "email";//Specification.getPropertyNameForWhereClause(Employee.class, "email");
+    private static final String EMPLOYEE_EMAIL = "email";
     private String email;
 
     public GetEmployeeByEmailSpecification(String email) {
@@ -22,8 +18,6 @@ public class GetEmployeeByEmailSpecification implements DetachedCriteriaSpecific
 
     @Override
     public DetachedCriteria query() {
-
-
         return DetachedCriteria.forClass(Employee.class).add(Restrictions.eq(EMPLOYEE_EMAIL, email));
     }
 }
