6f75eac674eaba28c313efef6f8315396d817c07
Andrii <aabrasha@gmail.com>
2016-06-08 17:33:49 +0300

    + java config for Persistence module (prod, test profiles) (#137)
    + profiles mapping for all module
    + persistence properties
    + default profile for application context
    + pgs syntax in create_db.sql
    - xml config for Persistence module

--- web/src/test/java/AntiVandalDtoTest.java
+++ web/src/test/java/AntiVandalDtoTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -38,6 +39,7 @@ import static org.junit.Assert.assertTrue;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring/web-test-ctx.xml")
+@ActiveProfiles("test")
 public class AntiVandalDtoTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AntiVandalDtoTest.class);
