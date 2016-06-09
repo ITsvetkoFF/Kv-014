e607e17d74d217f289fa134071452fb9d64cf974
Julia Avdeionok <julia@avdeionok.org.ua>
2016-06-09 14:35:18 +0300

    Add persistence, service, web layer for US01- lists of tasks - user story

---
+++ persistence/src/main/java/edu/softserve/zoo/persistence/specification/impl/task/ParametrizedTasksSpecification.java
@@ -0,0 +1,38 @@
+package edu.softserve.zoo.persistence.specification.impl.task;
+
+import edu.softserve.zoo.model.Task;
+import edu.softserve.zoo.persistence.specification.hibernate.DetachedCriteriaSpecification;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+
+/**
+ * <p>Implementation of {@link DetachedCriteriaSpecification} for filtering {@link Task} with nested entities</p>
+ *
+ * @param <T> the type of the properties
+ * @author Julia Avdeionok
+ */
+class ParametrizedTasksSpecification<T> implements DetachedCriteriaSpecification<Task> {
+    private String fieldName;
+    private T fieldValue;
+
+    /**
+     * @param fieldName  define filtering properties name
+     * @param fieldValue define filtering properties value
+     */
+    ParametrizedTasksSpecification(String fieldName, T fieldValue) {
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    @Override
+    public DetachedCriteria query() {
+
+        DetachedCriteria criteria = DetachedCriteria.forClass(Task.class, "task");
+        criteria.createAlias("zone", "zone")
+                .createAlias("assigner", "assigner")
+                .createAlias("assignee", "assignee")
+                .add(Restrictions.eq(fieldName, fieldValue));
+        return criteria;
+    }
+
+}
---
+++ service/src/main/java/edu/softserve/zoo/service/impl/TaskServiceImpl.java
@@ -0,0 +1,66 @@
+package edu.softserve.zoo.service.impl;
+
+import edu.softserve.zoo.exceptions.ApplicationException;
+import edu.softserve.zoo.exceptions.NotFoundException;
+import edu.softserve.zoo.model.Employee;
+import edu.softserve.zoo.model.Task;
+import edu.softserve.zoo.persistence.repository.Repository;
+import edu.softserve.zoo.persistence.repository.TaskRepository;
+import edu.softserve.zoo.persistence.specification.impl.task.TaskGetAllByAssigneeIdSpecification;
+import edu.softserve.zoo.persistence.specification.impl.task.TaskGetAllByAssignerIdSpecification;
+import edu.softserve.zoo.service.EmployeeService;
+import edu.softserve.zoo.service.TaskService;
+import edu.softserve.zoo.service.exception.ServiceReason;
+import edu.softserve.zoo.util.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * <p> Service implementation of {@link TaskService} for domain object {@link Task}</p>
+ *
+ * @author Julia Avdeionok
+ */
+@Service
+public class TaskServiceImpl extends AbstractService<Task> implements TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Override
+    Repository<Task> getRepository() {
+        return taskRepository;
+    }
+
+    @Override
+    @Transactional
+    public List<Task> taskGetAllByAssignerId(Long assignerId) {
+        Validator.notNull(employeeService.findOne(assignerId, Employee.class),
+                ApplicationException
+                        .getBuilderFor(NotFoundException.class)
+                        .forReason(ServiceReason.NOT_FOUND)
+                        .withMessage("Invalid Employee id supplied")
+                        .build());
+
+        return taskRepository.find(new TaskGetAllByAssignerIdSpecification(assignerId));
+    }
+
+    @Override
+    @Transactional
+    public List<Task> taskGetAllByAssigneeId(Long assigneeId) {
+        Validator.notNull(employeeService.findOne(assigneeId, Employee.class),
+                ApplicationException
+                        .getBuilderFor(NotFoundException.class)
+                        .forReason(ServiceReason.NOT_FOUND)
+                        .withMessage("Invalid Employee id supplied")
+                        .build());
+
+        return taskRepository.find(new TaskGetAllByAssigneeIdSpecification(assigneeId));
+    }
+
+}
