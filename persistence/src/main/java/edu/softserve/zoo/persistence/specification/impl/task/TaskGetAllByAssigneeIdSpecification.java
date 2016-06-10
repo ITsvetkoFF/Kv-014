c584b656e76cb7eb995eb092471ee88ef06e47a7
JuliaAvdeionok <JuliaAvdeionok@users.noreply.github.com>
2016-06-10 09:04:10 +0300

    Add persistence, service, web layer for US01- lists of tasks user story (#145)

---
+++ persistence/src/main/java/edu/softserve/zoo/persistence/specification/impl/task/TaskGetAllByAssigneeIdSpecification.java
@@ -0,0 +1,20 @@
+package edu.softserve.zoo.persistence.specification.impl.task;
+
+import edu.softserve.zoo.model.Employee;
+import edu.softserve.zoo.model.Task;
+
+/**
+ * <p>Implementation of for filtering {@link Task} by assigneeId parameter</p>
+ *
+ * @author Julia Avdeionok
+ */
+public class TaskGetAllByAssigneeIdSpecification  extends ParametrizedTasksSpecification<Long> {
+    static private String fieldName ="assignee.id";
+
+    /**
+     * @param assigneeId {@link Employee} define filtering properties value
+     */
+    public TaskGetAllByAssigneeIdSpecification(Long assigneeId) {
+        super(fieldName, assigneeId);
+    }
+}
