8befb11bc9e5fca211e6f95ebc8552448e642396
mononofuu <mononofuu@gmail.com>
2016-07-06 16:38:34 +0300

    import added

--- web/src/main/java/edu/softserve/zoo/advice/EmployeeResponseAdvice.java
+++ web/src/main/java/edu/softserve/zoo/advice/EmployeeResponseAdvice.java
@@ -1,5 +1,6 @@
 package edu.softserve.zoo.advice;
 
+import edu.softserve.zoo.controller.rest.EmployeeRestController;
 import edu.softserve.zoo.controller.rest.UserController;
 import edu.softserve.zoo.dto.EmployeeDto;
 import edu.softserve.zoo.dto.RoleDto;
